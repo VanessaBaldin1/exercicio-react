6a3306b7ae8cce1bf1a734f2d51633a76739ffe8
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-27 12:42:22 -0300

    Adiciona CSS modulo no Cabeçalho

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,11 +1,12 @@
 import logo from "../assets/logo-react.png";
-import Menu from "./Menu";
+import Menu from "../Menu/Menu";
+import estilos from "../Cabecalho/Cabecalho.module.css";
 
 export default function Cabecalho() {
   return (
-    <header>
-      <h1>
-        <img src={logo} alt="" /> Design
+    <header className={ estilos.toposite}>
+      <h1 className={ estilos.titulo}>
+        <img  className={ estilos.logo} src={logo} alt="" /> Design
       </h1>
       <hr />
 
