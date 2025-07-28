0aa096087651d887350d479e6d622d7df319e484
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-28 09:50:26 -0300

    ajuste no estilo do rodape

--- src/components/Rodape/Rodape.tsx
+++ src/components/Rodape/Rodape.tsx
@@ -1,23 +1,11 @@
-import type { CSSProperties } from "react";
-
-const estilosRodape: CSSProperties = {
-  backgroundColor: "#000",        // cor de fundo preta
-  color: "white",
-  textAlign: "center",
-  padding: "0.5rem",              // padding menor para diminuir a altura
-  fontSize: "0.9rem",             // tamanho de fonte um pouco menor (opcional)
-};
+import estilos from "./Rodape.module.css";
 
 export default function Rodape() {
   return (
-    <footer style={estilosRodape}>
-      <h2 style={{ margin: "0", fontSize: "1rem" }}>Pequeno site criado com React</h2>
-      <p style={{ color: "blue", fontWeight: "bold", margin: "0.2rem 0" }}>
-        Desenvolvido por{" "}
-        <b>
-         Vanessa
-        </b>{" "}
-        &copy;2025
+    <footer className={estilos.rodape}>
+      <h2>Pequeno site criado com React</h2>
+      <p>
+        Desenvolvido por <b>Vanessa</b> &copy;2025
       </p>
     </footer>
   );
