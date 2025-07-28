46839a405436e87db6413b439b01c9cdbc487cdf
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-28 11:29:52 -0300

    Adiciona useLocation no menu

--- src/components/Cabecalho/Menu/Menu.tsx
+++ src/components/Cabecalho/Menu/Menu.tsx
@@ -1,24 +1,57 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 import estilos from "../Menu/Menu.module.css";
 
+const Menu = () => {
+  const location = useLocation();
 
-export default function Menu() {
-  return (
-    <nav className={estilos.nav}>
-      <ul className={estilos.navList}>
-        <li className={estilos.navItem}>
-          <Link to="/" className={estilos.link}>Home</Link>
-        </li>
-        <li className={estilos.navItem}>
-          <Link to="/sobre" className={estilos.link}>Sobre</Link>
-        </li>
-        <li className={estilos.navItem}>
-          <Link to="/tipos-de-design" className={estilos.link}>Tipos de Design</Link>
-        </li>
-        <li className={estilos.navItem}>
-          <Link to="/ux-design" className={estilos.link}>Ux Design</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+  {
+    return (
+      <nav className={estilos.nav}>
+        <ul className={estilos.navList}>
+          <li className={estilos.navItem}>
+            <Link
+              to="/"
+              className={`${estilos.link} ${
+                location.pathname === "/" ? estilos.active : " "
+              }`}
+            >
+              Home
+            </Link>
+          </li>
+          <li className={estilos.navItem}>
+            <Link
+              to="/sobre"
+              className={`${estilos.link} ${
+                location.pathname === "/sobre" ? estilos.active : " "
+              }`}
+            >
+              Sobre
+            </Link>
+          </li>
+          <li className={estilos.navItem}>
+            <Link
+              to="/tipos-de-design"
+              className={`${estilos.link} ${
+                location.pathname === "/tipos-de-design" ? estilos.active : " "
+              }`}
+            >
+              Tipos de Design
+            </Link>
+          </li>
+          <li className={estilos.navItem}>
+            <Link
+              to="/ux-design"
+              className={` ${estilos.link} ${
+                location.pathname === "/ux-design" ? estilos.active : " "
+              }`}
+            >
+              Ux Design
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+};
+
+export default Menu;
